18678826e9f6c58c099ff1e1c2e6ff60d939f657
Florian Mauduit <flotix@linux.com>
2015-11-19 11:49:07 +0100

    Remove WP steps

--- bin/run.sh
+++ bin/run.sh
@@ -76,15 +76,6 @@ if [ ! -d ${PHP_SESSION_PATH} ]; then
    chown www-data:www-data ${PHP_SESSION_PATH}
 fi
 
-if [ ! -e ${HTTP_DOCUMENTROOT}/index.php ]; then
-   echo "=> Installing wordpress in ${HTTP_DOCUMENTROOT} - this may take a while ..."
-   touch ${HTTP_DOCUMENTROOT}/index.php
-   curl -o /tmp/wordpress.tar.gz "https://wordpress.org/wordpress-${WORDPRESS_VERSION}.tar.gz"
-   tar -zxf /tmp/wordpress.tar.gz -C /tmp/
-   mv /tmp/wordpress/* ${HTTP_DOCUMENTROOT}/
-   chown -R www-data:www-data ${HTTP_DOCUMENTROOT}
-fi
-
 if grep "PXC nodes here" /etc/haproxy/haproxy.cfg >/dev/null; then
    PXC_HOSTS_HAPROXY=""
    PXC_HOSTS_COUNTER=0
@@ -101,51 +92,7 @@ if grep "PXC nodes here" /etc/haproxy/haproxy.cfg >/dev/null; then
    perl -p -i -e "s/# PXC nodes here.*/# PXC nodes here\n${PXC_HOSTS_HAPROXY}/g" /etc/haproxy/haproxy.cfg
 fi
 
-if [ ! -e ${HTTP_DOCUMENTROOT}/wp-config.php ] && [ -e ${HTTP_DOCUMENTROOT}/wp-config-sample.php ] ; then
-   echo "=> Configuring wordpress..."
-   touch ${HTTP_DOCUMENTROOT}/wp-config.php
-   WP_DB_PASSWORD=`pwgen -s 20 1`
-   sed -e "s/database_name_here/$WP_DB_NAME/
-   s/username_here/$WP_DB_NAME/
-   s/password_here/$WP_DB_PASSWORD/
-   s/localhost/127.0.0.1/
-   /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-   /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-   /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-   /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-   /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-   /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-   /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-   /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" ${HTTP_DOCUMENTROOT}/wp-config-sample.php > ${HTTP_DOCUMENTROOT}/wp-config.php
-   chown www-data:www-data ${HTTP_DOCUMENTROOT}/wp-config.php
-   chmod 640 ${HTTP_DOCUMENTROOT}/wp-config.php
 
-  # Download nginx helper plugin
-  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
-  unzip -o nginx-helper.*.zip -d ${HTTP_DOCUMENTROOT}/wp-content/plugins
-  chown -R www-data:www-data ${HTTP_DOCUMENTROOT}/wp-content/plugins/nginx-helper
-
-  # Activate nginx plugin and set up pretty permalink structure once logged in
-  cat << ENDL >> ${HTTP_DOCUMENTROOT}/wp-config.php
-\$plugins = get_option( 'active_plugins' );
-if ( count( \$plugins ) === 0 ) {
-  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
-  \$wp_rewrite->set_permalink_structure( '/%postname%/' );
-  \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
-  foreach ( \$pluginsToActivate as \$plugin ) {
-    if ( !in_array( \$plugin, \$plugins ) ) {
-      activate_plugin( '${HTTP_DOCUMENTROOT}/wp-content/plugins/' . \$plugin );
-    }
-  }
-}
-ENDL
-
-  echo "=> Creating database ${WP_DB_NAME}, username ${WP_DB_NAME}, with password ${WP_DB_PASSWORD} ..."
-  service haproxy start
-  sleep 2
-  mysql -h 127.0.0.1 -u root -p${DB_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME}; GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_NAME}'@'10.42.%' IDENTIFIED BY '${WP_DB_PASSWORD}'; FLUSH PRIVILEGES;"
-  service haproxy stop
-fi
 
 if [ ! -e ${HTTP_DOCUMENTROOT}/healthcheck.txt ]; then
    echo "OK" > ${HTTP_DOCUMENTROOT}/healthcheck.txt
